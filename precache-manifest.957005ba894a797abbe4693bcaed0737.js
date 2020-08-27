9c84baf979bcfc9f7c51519d742a90a048158bb8
Max <narrm4517@gmail.com>
2020-08-27 22:36:02 +0300

    init

---
+++ precache-manifest.957005ba894a797abbe4693bcaed0737.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec4da1c3ab696fca587165075af861c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00c9cb233b0516f31f51",
+    "url": "/static/css/main.b96a1bbf.chunk.css"
+  },
+  {
+    "revision": "cd61ec61a8eb2d0182f8",
+    "url": "/static/js/2.6777b642.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6777b642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c9cb233b0516f31f51",
+    "url": "/static/js/main.407f484d.chunk.js"
+  },
+  {
+    "revision": "94ed7c61c74350c421b8",
+    "url": "/static/js/runtime-main.50640541.js"
+  },
+  {
+    "revision": "27605d7cc1fcfbc2ef41340617f0d013",
+    "url": "/static/media/OpenSans-Regular.27605d7c.woff"
+  },
+  {
+    "revision": "fe42f092865d178997258c80c24a5703",
+    "url": "/static/media/OpenSans-SemiBold.fe42f092.woff"
+  },
+  {
+    "revision": "611c9b2d5150900ffbbc2d1a31c3b61d",
+    "url": "/static/media/PTSans-Regular.611c9b2d.woff"
+  },
+  {
+    "revision": "49d8d8285e8bc3047dac602de489bfb0",
+    "url": "/static/media/SourceSansPro-Regular.49d8d828.woff"
+  },
+  {
+    "revision": "3c88af2913c75e1a8b89437b2ba893a5",
+    "url": "/static/media/banner-photo.3c88af29.webp"
+  },
+  {
+    "revision": "820a9898deeb27311239e3f62d79a64e",
+    "url": "/static/media/banner-photo@2x.820a9898.webp"
+  },
+  {
+    "revision": "bce28fb772838de34344b20fa26296b0",
+    "url": "/static/media/logo.bce28fb7.svg"
+  },
+  {
+    "revision": "f28cb247d5778a3e9e6e5be021b9a81b",
+    "url": "/static/media/man-laptop.f28cb247.svg"
+  },
+  {
+    "revision": "63637e948daaff9637f3f5a803d2b669",
+    "url": "/static/media/menu-icon.63637e94.svg"
+  },
+  {
+    "revision": "a33f2d3cee0ddafd303461d319e9a2ab",
+    "url": "/static/media/photo-cover.a33f2d3c.svg"
+  }
+]);
